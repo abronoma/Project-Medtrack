551e3d87a5203f9e410085048eb075bfb494845e
Ayebs <korantengsarah33@gmail.com>
2024-01-31 10:28:55 +0000

    React icons added

--- src/components/pharmacy/Table.jsx
+++ src/components/pharmacy/Table.jsx
@@ -1,29 +1,41 @@
-import React from 'react'
+import React from "react";
+import { pharmacyData } from "../../data/dummy.js";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { FaRegEdit } from "react-icons/fa";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+
 
 export default function Table() {
   return (
-    <div className='table'>
-        <table>
-            <thead>
-                <tr>
-                  <th>Drug Name</th>
-                  <th>Description</th>
-                  <th>Unit of Pricing</th>
-                  <th>Drug Code</th>
-                  <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td><button>DELETE</button></td>
-                </tr>
-            </tbody>
-        </table>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Drug Name</th>
+            <th>Description</th>
+            <th>Unit of Pricing</th>
+            <th>Drug Code</th>
+            <th>Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pharmacyData.map((item) => (
+            <tr key={item.id}>
+              <td>{item.drugName}</td>
+              <td>{item.description}</td>
+              <td>{item.unitOfPricing}</td>
+              <td>{item.drugCode}</td>
+              <td>{item.price}</td>
+              <td>
+                <button><MdOutlineRemoveRedEye/></button>
+                <button><RiDeleteBin6Line/></button>
+                <button><FaRegEdit/></button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
